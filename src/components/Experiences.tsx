74f7ec9e2ae06d7d12bd0d4d80c02d55f12cbc05
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-01-28 12:54:25 -0300

    feat: created Experiences component

---
+++ src/components/Experiences.tsx
@@ -0,0 +1,9 @@
+export function Experiences() {
+  return (
+    <div className=" text-center p-10 ">
+      <h2 className=" text-5xl py-2 ">Experiências</h2>
+
+      <p className=" text-md py-2 "></p>
+    </div>
+  );
+}
