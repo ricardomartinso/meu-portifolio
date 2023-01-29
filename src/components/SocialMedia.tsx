2cb7a95f608be9d9561e43f10b01635b97e3993d
ricardomartinso <ricardomartinso.dev@gmail.com>
2023-01-29 18:02:21 -0300

    feat: added framer motion to social media icons

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,37 +1,42 @@
 import { AiFillLinkedin, AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 export function SocialMedia(): JSX.Element {
   return (
-    <div
+    <motion.div
+      initial={{ x: -500, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ duration: 1.5 }}
       id="redes-sociais"
       className="
       flex 
       items-center 
       justify-center 
-      gap-8 text-4xl 
-      text-primary"
+      gap-8 text-2xl 
+      text-white"
     >
-      <a
+      <Link
         href="https://www.linkedin.com/in/ricardomartinso/"
         target="_blank"
         rel="noreferrer"
       >
         <AiFillLinkedin />
-      </a>
-      <a
+      </Link>
+      <Link
         href="https://www.github.com/ricardomartinso"
         target="_blank"
         rel="noreferrer"
       >
         <AiFillGithub />
-      </a>
-      <a
+      </Link>
+      <Link
         href="https://www.instagram.com/ricardoomartins_"
         target="_blank"
         rel="noreferrer"
       >
         <AiFillInstagram />
-      </a>
-    </div>
+      </Link>
+    </motion.div>
   );
 }
